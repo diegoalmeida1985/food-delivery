cc789ca1ed145637a3b6b2b50e3814d7170fe5cd
Diego Almeida <diegoalmeida1985@gmail.com>
2023-06-06 19:31:56 -0300

    Added a warning screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Home from './Components/Home.js'
+import Home from './Components/Home.js';
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home/>
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,52 @@
+import { Box } from "@mui/material";
+import { Grid } from "@mui/material";
+import {Card} from "@mui/material";
+import stripes from "./images/stripes.jpg";
+import background from "./images/construction-bg.png";
+
+
 function Home() {
     return (
-        <div>
-            <h1>THIS IS A FOOD DELIVERY WEB APP</h1>
-        </div>
+        <Box 
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+            <Grid 
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100vw', 
+                    height: '100vh',
+                    backgroundImage: `url(${background})`,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    
+                }}>
+                <Card 
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundImage: `url(${stripes})`,
+                        backgroundRepeat: 'repeat',
+                        backgroundSize: 'contain',
+                        width: '100%',
+                        height: '20%',
+                        color: "black",
+                        fontSize: 'calc(3vw + 1.5vmin)',
+                        boxShadow: '0px 5px 20px 5px black',
+                        
+                    }}>
+                    THIS PROJECT IS STILL UNDER DEVELOPMENT
+                </Card>
+            </Grid>
+        </Box>
     )
 }
 
